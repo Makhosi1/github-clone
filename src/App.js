1eed0f5ff0bc5cff9c570089f7244d35dc4ad940
Coco <admin@Makhosi-MacBook-Pro.local>
2023-12-14 18:30:48 +0000

    removed some files and added some folders and installation ffiles

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Search from "./components/search/Search";
+
+function App() {
+  return (
+   <>
+   <Search />
+   <h1>hello project</h1>
+   </>
+  );
+}
+
+export default App;
---
+++ src/components/search/Search.js
@@ -0,0 +1,9 @@
+
+export default function Search(){
+    return (
+        <form>
+            <input/>
+           <button>Submit</button>
+        </form>
+    )
+};
